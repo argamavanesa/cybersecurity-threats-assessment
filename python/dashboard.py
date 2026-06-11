fcdb1e5785cdc7956cb7ac2953a4a04215ed42a1
Argama Vanesa Nauli Sijabat <argama.vanesa@ui.ac.id>
2026-06-11 21:25:22 +0700

    Update dashboard.py

--- python/dashboard.py
+++ python/dashboard.py
@@ -4,7 +4,7 @@ import plotly.express as px
 
 st.set_page_config(layout="wide", page_title="Cybersecurity Threat Assessment")
 
-df = pd.read_csv('../data/raw-data.csv')
+df = pd.read_csv('/data/raw-data.csv')
 df['Financial Loss (in Billion $)'] = df['Financial Loss (in Million $)'] / 1000
 
 # ==========================================
